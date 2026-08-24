a0b2104e966d0851e2badd483906488c8067b95d
Vivolo Finol <fabiola-maria.vivolo-finol@serco.com>
2026-08-24 14:36:00 +0200

    ft_strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <stdio.h>
+
+
+size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+    size_t i;
+
+    i = 0;
+    while (src[i] != '\0')
+{
+        if(i + 1 < dstsize){
+        dst[i] = src[i];}
+        i++;
+    }
+    if(dstsize > 0){
+        dst[dstsize - 1] = '\0';
+    }
+    
+    return(i);
+
+}
+
+int main(){
+    char dest[10] = "Epa";
+    char src[10] = "Hey!";
+
+    printf("%zu", ft_strlcpy(dest, src, 3));
+
+    return 0;
+}
