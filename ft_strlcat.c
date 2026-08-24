820cfd2928161dc185257c48deeae9a7a8ed070f
Fabiola Vivolo <fvivolo@c2r4s5.42prague.com>
2026-08-24 18:48:09 +0200

    worked in 42

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,44 +1,49 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fvivolo <fvivolo@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/08/24 15:58:22 by fvivolo           #+#    #+#             */
-/*   Updated: 2026/08/24 15:58:57 by fvivolo          ###   ########.fr       */
+/*   Created: 2026/08/24 16:02:24 by fvivolo           #+#    #+#             */
+/*   Updated: 2026/08/24 18:47:28 by fvivolo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
+	size_t	j;
 
 	i = 0;
-	while (src[i] != '\0')
+	j = 0;
+	while (dst[i] != '\0')
 	{
-		if (i + 1 < dstsize)
-		{
-			dst[i] = src[i];
-		}
 		i++;
 	}
-	if (dstsize > 0)
+	while (src[j] != '\0')
 	{
-		dst[dstsize - 1] = '\0';
+		if (i + j + 1 < size)
+			dst[i + j] = src[j];
+		j++;
 	}
-	return (i);
+	if (size > 0)
+	{
+		dst[i + 1] = '\0';
+		i++;
+	}
+	return (i + j);
 }
 
-// int	main(void)
-// {
-// 	char dest[10] = "Epa";
-// 	char src[10] = "Hey!";
+int	main(void)
+{
+	char dest[10] = "o";
+	char src[10] = "W";
 
-// 	printf("%zu", ft_strlcpy(dest, src, 3));
+	printf("%zu", ft_strlcat(dest, src, 16));
 
-// 	return (0);
-// }
+	return (0);
+}
