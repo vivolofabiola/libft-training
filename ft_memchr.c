931ba05e66009b82b9539e068499ab5a0c3dcbb8
Fabiola Vivolo <vivolofabiola@gmail.com>
2026-08-28 14:52:21 +0200

    yay, memchr.c wasnt so bad

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,1 +1,17 @@
 #include "libft.h"
+
+void *ft_memchr(const void *s, int c, size_t n){
+
+unsigned char *ptr;
+ptr = (unsigned char *)s;
+size_t i = 0;
+
+while(i < n){
+    if (ptr[i] == (unsigned char)c){
+       return((unsigned char *)&ptr[i]);
+    }
+    i++;
+}
+
+return(NULL);
+}
