02d499006ae5223781d7a8a3993dfee83c1fcea3
Fabiola Vivolo <fvivolo@c2r7s3.42prague.com>
2026-09-01 18:20:35 +0200

    libfth.h updates

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: fvivolo <fvivolo@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/01 17:51:17 by fvivolo           #+#    #+#             */
-/*   Updated: 2026/09/01 17:52:26 by fvivolo          ###   ########.fr       */
+/*   Updated: 2026/09/01 18:15:41 by fvivolo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	ft_isalnum(int c)
 {
-	return ((c >= '0' && c <= '9' || c >= 'a' && c <= 'z' || c >= 'A'
-			&& c <= 'Z'));
+	return (((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A'
+				&& c <= 'Z')));
 	return (ft_isalpha(c) || ft_isdigit(c));
 }
 // int main(){
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: fvivolo <fvivolo@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/01 16:58:54 by fvivolo           #+#    #+#             */
-/*   Updated: 2026/09/01 17:49:34 by fvivolo          ###   ########.fr       */
+/*   Updated: 2026/09/01 18:13:52 by fvivolo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,37 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	size_t start = 0;
+	size_t end;
+	size_t len = 0;
+
+	ft_strlen(s1);
+
+	ft_strchr(set, s1[start]);
+
+	while (ft_strlen(s1) && ft_strchr(set, strlen(s1)))
+	{
+		start++;
+	}
+
+	end = ft_strlen(s1);
+
+	while (end && ft_strchr(set, strlen(s1)))
+	{
+		end--;
+	}
+
+	if (s1 == set)
+	{
+		return ('\0');
+	}
+
+	char *new_str = malloc((s1[start] + s1[end] + 1));
+
+	if (new_str == NULL)
+	{
+		return (NULL);
+	}
+
+	new_str[start + end] = '\0';
 }
