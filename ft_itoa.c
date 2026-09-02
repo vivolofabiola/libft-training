117e1679887fb3610c569e106b766285c88bf797
Fabiola Vivolo <fvivolo@c2r6s2.42prague.com>
2026-09-02 19:59:25 +0200

    last  from 42

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,3 +1,20 @@
 #include "libft.h"
 
-char	*ft_itoa(int n);
+char	*ft_itoa(int n){
+
+	int count = 0;
+
+	if(n == -2147483647){
+		write(1, -2147483647, 11);
+	} else if(){}
+
+	while(n != 0){
+		n = n / 10;
+		count = count + 1;
+
+	}
+
+
+
+
+}
