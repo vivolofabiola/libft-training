b193f26735edfbcaf200e1b59eb8c1b14b34cded
Fabiola Vivolo <fvivolo@c2r7s1.42prague.com>
2026-09-04 20:03:00 +0200

    working on it

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fvivolo <fvivolo@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/04 17:44:09 by fvivolo           #+#    #+#             */
-/*   Updated: 2026/09/04 17:44:11 by fvivolo          ###   ########.fr       */
+/*   Updated: 2026/09/04 19:47:49 by fvivolo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,6 @@ void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
-t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
-						void (*del)(void *));
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *));
 
 #endif
